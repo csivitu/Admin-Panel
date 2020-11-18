9c60ed29769a49510d6f55ba2d636d62b25d13bd
alias-rahil <rahil.kabani.4@gmail.com>
2020-11-19 02:37:36 +0530

    feat: RESTful api

--- services/event/event.js
+++ services/event/event.js
@@ -26,6 +26,37 @@ module.exports = {
         ctx.meta.$statusCode = 400
         return { error: err.toString() }
       }
+    },
+    get (ctx) {
+      try {
+        return Event.findOne({ name: ctx.params.id }, { _id: false })
+      } catch (err) {
+        ctx.meta.$statusCode = 400
+        return { error: err.toString() }
+      }
+    },
+    update (ctx) {
+      try {
+        const { name, dbType, dbURL, date } = ctx.params
+        if (dbType !== 'SQL' || dbType !== 'NOSQL') {
+          ctx.meta.$statusCode = 400
+          return { error: 'Error: invalid db type' }
+        }
+        return Event.updateOne({
+          name
+        }, { name, dbType, dbURL, date })
+      } catch (err) {
+        ctx.meta.$statusCode = 400
+        return { error: err.toString() }
+      }
+    },
+    delete (ctx) {
+      try {
+        return Event.deleteOne({ name: ctx.params.id })
+      } catch (err) {
+        ctx.meta.$statusCode = 400
+        return { error: err.toString() }
+      }
     }
   }
 }
