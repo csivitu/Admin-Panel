1a66f721839f991399c24d36b1996b1a751de88e
alias-rahil <rahil.kabani.4@gmail.com>
2020-11-20 22:04:48 +0530

    fix: use connection string directly

--- db/connectSql.js
+++ db/connectSql.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql')
-const { ConnectionString } = require('connection-string')
 
 function connectSql (dbURL) {
   return new Promise((resolve, reject) => {
-    const obj = new ConnectionString(dbURL)
-    const connection = mysql.createConnection(obj)
+    const connection = mysql.createConnection(dbURL)
     connection.connect((e) => {
       if (e) {
         reject(e)
