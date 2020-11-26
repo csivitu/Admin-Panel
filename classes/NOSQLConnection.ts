cb45dd7d56a42b2e1116ff56602f36af74b79c37
alias-rahil <rahil.kabani.4@gmail.com>
2020-11-26 12:05:19 +0530

    fix: remove comments

--- classes/NOSQLConnection.ts
+++ classes/NOSQLConnection.ts
@@ -48,28 +48,6 @@ export default class NOSQLDBConnection extends DBConnection {
       });
     }
 
-    // updateOne(collection: string, name: string, doc: object): Promise<object> {
-    //   return new Promise((resolve, reject) => {
-    //     this.connection?.db.collection(collection)
-    //       .updateOne({ name }, doc).then((updatedDocument: object) => {
-    //         resolve(updatedDocument);
-    //       }).catch((error: object) => {
-    //         reject(error);
-    //       });
-    //   });
-    // }
-
-    // deleteOne(collection: string, name: string): Promise<object> {
-    //   return new Promise((resolve, reject) => {
-    //     this.connection?.db.collection(collection)
-    //       .deleteOne({ name }).then((deletedDocument: object) => {
-    //         resolve(deletedDocument);
-    //       }).catch((error: object) => {
-    //         reject(error);
-    //       });
-    //   });
-    // }
-
     listCollections(): Promise<object> {
       return new Promise((resolve, reject) => {
         this.connection?.db.listCollections().toArray((error: object,
