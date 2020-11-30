e29a176241755a08112e65d9d7e70e7563f95505
ashikka <ashikagupta28@gmail.com>
2020-11-30 16:54:22 +0530

    feat: Added export methods

--- src/export/export.ts
+++ src/export/export.ts
@@ -3,7 +3,7 @@ import { liveConnections } from '../../db/connectProjects';
 import { ContextSchema } from '../../interfaces/interfaces';
 
 async function methodWrapper(ctx: ContextSchema, method: string) {
-    const { project, collection } = ctx.params;
+    const { project, collection, key } = ctx.params;
     if (!liveConnections[project]) {
         ctx.meta.$statusCode = 400;
         return { error: 'Error: invalid project or could not connect to project database' };
@@ -11,6 +11,14 @@ async function methodWrapper(ctx: ContextSchema, method: string) {
     try {
         if (method === 'listCollections') {
             const collections = await liveConnections[project].listCollections();
+            return collections;
+        }
+        if (method === 'deleteCollection') {
+            const collections = await liveConnections[project].deleteCollection(collection);
+            return collections;
+        }
+        if (method === 'deleteDocument') {
+            const collections = await liveConnections[project].deleteDocument(collection, key);
             return collections;
         }
         const data = await liveConnections[project].exportCollection(collection);
@@ -29,6 +37,12 @@ broker.createService({
         },
         exportCollection(ctx) {
             return methodWrapper(ctx, 'exportCollection');
+        },
+        deleteCollection(ctx) {
+            return methodWrapper(ctx, 'deleteCollection');
+        },
+        deleteDocument(ctx) {
+            return methodWrapper(ctx, 'deleteDocument');
         },
     },
 });
